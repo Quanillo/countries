7b5896bfa5bbded06709c0f48e35fc7ef9b70fe0
juan <j.garrigos91@gmail.com>
2022-11-29 02:13:30 +0100

    limpiando

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css';
-//import { Filter } from './Components/Filter';
 import { Countries } from './Components/Countries';
 import { CountrieInfo} from './Components/CountrieInfo';
 
 function App() {
   const [ countries, setCountries ] = useState([])
-  //const [ filterList2, setFilterList2 ] = useState([])
   const [ filter, setFilter ] = useState('')
 
   useEffect(() => {
@@ -18,21 +16,22 @@ function App() {
       })
   }, [])
 
-  /*
-  const handleChangeFilter = (event) => {
-    let filter = event.target.value
-    setFilter(event.target.value)
-  
-    const isInFilter = countries.filter(function (countrie){
-      return countrie.name.official.toUpperCase().includes(filter.toUpperCase())
-    })
-    
-    setFilterList2(isInFilter)
-  }
-  **/
   const isInFilter = countries.filter(function (countrie){
     return countrie.name.official.toUpperCase().includes(filter.toUpperCase())
   })
+  
+  const Restult = ({isInFilter}) => {
+    if(isInFilter.length===1){
+      const countrie = isInFilter[0];
+      return(
+      <CountrieInfo countrie={countrie} />
+      )
+    }
+    else{
+      return <Countries  isInFilter={isInFilter} />
+    }    
+    
+  }
   
   return (
     <div className="App">
@@ -43,23 +42,5 @@ function App() {
     </div>
   );
 }
-
-const Restult = ({isInFilter}) => {
-  if(isInFilter.length===1){
-    const countrie = isInFilter[0];
-    return(
-    <CountrieInfo countrie={countrie} />
-    )
-  }
-  if (isInFilter > 10){
-    return <p>Too many matches, sepecify another filter</p>  
-  }
-  
-  return <Countries  isInFilter={isInFilter} />
-}
-
-
-
-//<Filter filter={filter} handleChangeFilter={handleChangeFilter}/> 
 
 export default App;
